f5c07f1663fbe845fea753bc063c471dba4b92ff
sivaprasadgandepalli <84697701+sivaprasadgandepalli@users.noreply.github.com>
2022-08-06 15:16:08 +0530

    import java.util.*; class sm{     public static void main(String args[])     {         String str;         Scanner s=new Scanner(System.in);         str=s.nextLine();         System.out.println(str);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class sm{
+    public static void main(String args[])
+    {
+        String str;
+        Scanner s=new Scanner(System.in);
+        str=s.nextLine();
+        System.out.println(str);
+    }
+}
