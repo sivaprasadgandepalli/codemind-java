e9df723b182f3f8811c8c6cf1b20fbbc5fc3a819
sivaprasadgandepalli <84697701+sivaprasadgandepalli@users.noreply.github.com>
2022-08-07 11:56:35 +0530

    import java.util.*; class cap{     public static void main(String args[])     {         int t,s,b,c;         Scanner sc=new Scanner(System.in);         t=sc.nextInt();         s=sc.nextInt();         b=sc.nextInt();         c=2*t*s*b*512;         System.out.println(c);     } }

---
+++ Disk_Capacity.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class cap{
+    public static void main(String args[])
+    {
+        int t,s,b,c;
+        Scanner sc=new Scanner(System.in);
+        t=sc.nextInt();
+        s=sc.nextInt();
+        b=sc.nextInt();
+        c=2*t*s*b*512;
+        System.out.println(c);
+    }
+}
