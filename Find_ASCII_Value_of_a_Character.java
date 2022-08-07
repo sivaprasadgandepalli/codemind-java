38aacb57551058e3e9de6f692d8a6e010b961d19
sivaprasadgandepalli <84697701+sivaprasadgandepalli@users.noreply.github.com>
2022-08-07 12:14:39 +0530

    import java.util.*; class asc{     public static void main(String args[])     {         char c1,c2;         Scanner sc=new Scanner(System.in);         c1=sc.next().charAt(0);         int a1=(int)c1;         System.out.println(a1);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class asc{
+    public static void main(String args[])
+    {
+        char c1,c2;
+        Scanner sc=new Scanner(System.in);
+        c1=sc.next().charAt(0);
+        int a1=(int)c1;
+        System.out.println(a1);
+    }
+}
