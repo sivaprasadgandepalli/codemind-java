6db799ec115e2270dc753e7829ee03dcc9d45d68
sivaprasadgandepalli <84697701+sivaprasadgandepalli@users.noreply.github.com>
2022-08-13 15:48:24 +0530

    import java.util.*; class nor{     public static void main(String args[])     {         int n;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         if (n<3)         {             System.out.println(n-1);         }         else         {              System.out.println(2);         }     } }

---
+++ The_Pandemic.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class nor{
+    public static void main(String args[])
+    {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        if (n<3)
+        {
+            System.out.println(n-1);
+        }
+        else
+        {
+             System.out.println(2);
+        }
+    }
+}
