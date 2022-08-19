bf35243c0c94316f09d05321ef12dc706184c745
sivaprasadgandepalli <84697701+sivaprasadgandepalli@users.noreply.github.com>
2022-08-19 11:22:33 +0530

    import java.util.Scanner; class SNN {  public static void main(String args[])  {   int n,sum;   Scanner sc=new Scanner(System.in);   n=sc.nextInt();   sum=n*(n+1)/2;   System.out.println(sum);
      }
    }

---
+++ Find_sum_of_natural_numbers_.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class SNN
+{
+ public static void main(String args[])
+ {
+  int n,sum;
+  Scanner sc=new Scanner(System.in);
+  n=sc.nextInt();
+  sum=n*(n+1)/2;
+  System.out.println(sum);
+
+  }
+}
