9703e1d3a98ecd30ae3f7db7b91c232794821005
sivaprasadgandepalli <84697701+sivaprasadgandepalli@users.noreply.github.com>
2022-08-25 19:17:13 +0530

    import java.util.*; class bp {     public static void main(String args[])     {         int n,l,r,s=0,p=1;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         l=(int)Math.log10(n)+1;         for(int i=1;i<=l;i++)         {
                r=n%10;
                n=n/10;
                s+=r;
                p*=r;
            }
            System.out.println(p-s);
        }
    }

---
+++ Subtract_the_Product_and_Sum_of_Digits_of_an_Integer.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class bp
+{
+    public static void main(String args[])
+    {
+        int n,l,r,s=0,p=1;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        l=(int)Math.log10(n)+1;
+        for(int i=1;i<=l;i++)
+        {
+            
+            r=n%10;
+            n=n/10;
+            s+=r;
+            p*=r;
+        }
+        System.out.println(p-s);
+    }
+}
