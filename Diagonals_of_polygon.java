483f7991851138b14bf7502a4f2be604106f92c1
sivaprasadgandepalli <84697701+sivaprasadgandepalli@users.noreply.github.com>
2022-09-15 18:53:00 +0530

    import java.util.*; class diag {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,d;         n=sc.nextInt();         d=n*(n-3)/2;         System.out.println(d);     } }

---
+++ Diagonals_of_polygon.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class diag
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,d;
+        n=sc.nextInt();
+        d=n*(n-3)/2;
+        System.out.println(d);
+    }
+}
