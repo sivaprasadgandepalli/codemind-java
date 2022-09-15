81ab5e933dc21d4a1fe4af1f22e74fd9b2b005c3
sivaprasadgandepalli <84697701+sivaprasadgandepalli@users.noreply.github.com>
2022-09-15 21:33:18 +0530

    import java.util.*; class con {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int f;         float c;         f=sc.nextInt();        c=(float)(5*f-160)/9;         System.out.format("%.2f",c);     } }

---
+++ Fahrenheit_to_Celsius.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class con
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int f;
+        float c;
+        f=sc.nextInt();
+       c=(float)(5*f-160)/9;
+        System.out.format("%.2f",c);
+    }
+}
