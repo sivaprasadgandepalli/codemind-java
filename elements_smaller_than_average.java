6cde7b58061746d1467096ca4c30f88aa957ab1f
sivaprasadgandepalli <84697701+sivaprasadgandepalli@users.noreply.github.com>
2022-09-16 23:04:49 +0530

    import java.util.*; class avg {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,s=0,avg,k=0;         n=sc.nextInt();         int x[]=new int[n];         for(int i=1;i<n;i++)         {             x[i]=sc.nextInt();             s+=x[i];         }         avg=Math.floorDiv(s,n);         for(int j=0;j<n;j++)         {             if(x[j]<=avg)             {                 k++;             }         }         System.out.println(k);
        }
    }

---
+++ elements_smaller_than_average.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class avg
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,s=0,avg,k=0;
+        n=sc.nextInt();
+        int x[]=new int[n];
+        for(int i=1;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+            s+=x[i];
+        }
+        avg=Math.floorDiv(s,n);
+        for(int j=0;j<n;j++)
+        {
+            if(x[j]<=avg)
+            {
+                k++;
+            }
+        }
+        System.out.println(k);
+        
+    }
+}
