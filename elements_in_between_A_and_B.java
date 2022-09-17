bb700501431c34d9e3288ca33b39b8a7230554a5
sivaprasadgandepalli <84697701+sivaprasadgandepalli@users.noreply.github.com>
2022-09-17 19:39:47 +0530

    import java.util.*; class elementsab {  public static void main(String args[])  { 	Scanner sc=new Scanner(System.in);    int n,k,l,c=0; 	n=sc.nextInt(); 	int x[]=new int[n]; 	for(int i=0;i<n;i++) 	{ 		x[i]=sc.nextInt(); 	} 	k=sc.nextInt(); 	l=sc.nextInt(); 	for(int j=0;j<n;j++) 	{ 		if(x[j]>=k && x[j]<=l) 		{ 			System.out.print(x[j]+" "); 			c++; 		} 	} 	if(c==0) 	{ 		System.out.println(-1); 	}   } }

---
+++ elements_in_between_A_and_B.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class elementsab
+{
+ public static void main(String args[])
+ {
+	Scanner sc=new Scanner(System.in);
+   int n,k,l,c=0;
+	n=sc.nextInt();
+	int x[]=new int[n];
+	for(int i=0;i<n;i++)
+	{
+		x[i]=sc.nextInt();
+	}
+	k=sc.nextInt();
+	l=sc.nextInt();
+	for(int j=0;j<n;j++)
+	{
+		if(x[j]>=k && x[j]<=l)
+		{
+			System.out.print(x[j]+" ");
+			c++;
+		}
+	}
+	if(c==0)
+	{
+		System.out.println(-1);
+	}
+  }
+}
