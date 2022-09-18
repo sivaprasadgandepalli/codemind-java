f96ed0327c98e7bfafe07e7ce18ab781a7a61203
sivaprasadgandepalli <84697701+sivaprasadgandepalli@users.noreply.github.com>
2022-09-18 21:22:58 +0530

    import java.util.*; class elementsgrtavg {  public static void main(String args[])  { 	Scanner sc=new Scanner(System.in); 	int n,s=0; 	n=sc.nextInt(); 	int x[]=new int[n]; 	for(int i=0;i<n;i++) 	{ 		x[i]=sc.nextInt(); 		s+=x[i]; 	} 	int k=0; 	for(int j=0;j<n;j++) 	{ 	    if(x[j]%2!=0) 	    { 	        k=j; 	    } 	} 	System.out.println(k);  } }

---
+++ last_odd_number_index_position.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class elementsgrtavg
+{
+ public static void main(String args[])
+ {
+	Scanner sc=new Scanner(System.in);
+	int n,s=0;
+	n=sc.nextInt();
+	int x[]=new int[n];
+	for(int i=0;i<n;i++)
+	{
+		x[i]=sc.nextInt();
+		s+=x[i];
+	}
+	int k=0;
+	for(int j=0;j<n;j++)
+	{
+	    if(x[j]%2!=0)
+	    {
+	        k=j;
+	    }
+	}
+	System.out.println(k);
+ }
+}
