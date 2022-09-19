0a677ece4c71c9ac93f7f85ee2f8dc22f8208f9f
sivaprasadgandepalli <84697701+sivaprasadgandepalli@users.noreply.github.com>
2022-09-19 19:20:57 +0530

    import java.util.*; class elementsearch {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,c=0,a,b;         n=sc.nextInt();         int x[]=new int[n];         for(int i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         a=sc.nextInt();         b=sc.nextInt();         for(int j=0;j<n;j++)         {             if(x[j]<a || x[j]>b)             {                 System.out.print(x[j]+" ");                 c++;             }         }         if(c==0)         {             System.out.println(-1);         }     } }

---
+++ elements_not_in_between_A_and_B.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class elementsearch
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,c=0,a,b;
+        n=sc.nextInt();
+        int x[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        a=sc.nextInt();
+        b=sc.nextInt();
+        for(int j=0;j<n;j++)
+        {
+            if(x[j]<a || x[j]>b)
+            {
+                System.out.print(x[j]+" ");
+                c++;
+            }
+        }
+        if(c==0)
+        {
+            System.out.println(-1);
+        }
+    }
+}
