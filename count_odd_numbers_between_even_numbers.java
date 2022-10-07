acc9a765b045482a90bc8e60f286b5dea7563f3e
sivaprasadgandepalli <84697701+sivaprasadgandepalli@users.noreply.github.com>
2022-10-07 22:58:35 +0530

    import java.util.*; class countofelem {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,c=0;         n=sc.nextInt();         int x[]=new int[n];         for(int i=0;i<n;i++)         {             x[i]=sc.nextInt();         }        for(int i=1;i<n-1;i++)        {            if(x[i-1]%2==0 &&x[i+1]%2==0)            {                if(x[i]%2!=0)                {                    c++;                }            }        }        System.out.println(c);     } }

---
+++ count_odd_numbers_between_even_numbers.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class countofelem
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,c=0;
+        n=sc.nextInt();
+        int x[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+       for(int i=1;i<n-1;i++)
+       {
+           if(x[i-1]%2==0 &&x[i+1]%2==0)
+           {
+               if(x[i]%2!=0)
+               {
+                   c++;
+               }
+           }
+       }
+       System.out.println(c);
+    }
+}
