e894124f8380945720dfbaa6edd65770f60320bd
sivaprasadgandepalli <84697701+sivaprasadgandepalli@users.noreply.github.com>
2022-10-07 16:09:28 +0530

    import java.util.*; class elmsmascnt {     public static int count(int n,int arr[])     {         int c=0;         for(int i=0;i<arr.length;i++)         {             if(arr[i]==n)             {                 c++;             }         }         return c;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,k=0,s;         n=sc.nextInt();         int x[]=new int[n];         for(int i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         for(int j=0;j<n;j++)         {             s=count(x[j],x);             if(x[j]==s)             {                 x[j]=-99+j;                 k++;             }         }        System.out.print(k);
        }
    }

---
+++ value_same_as_count.java
@@ -0,0 +1,38 @@
+import java.util.*;
+class elmsmascnt
+{
+    public static int count(int n,int arr[])
+    {
+        int c=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]==n)
+            {
+                c++;
+            }
+        }
+        return c;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,k=0,s;
+        n=sc.nextInt();
+        int x[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        for(int j=0;j<n;j++)
+        {
+            s=count(x[j],x);
+            if(x[j]==s)
+            {
+                x[j]=-99+j;
+                k++;
+            }
+        }
+       System.out.print(k);
+        
+    }
+}
